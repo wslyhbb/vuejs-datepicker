eb5d9993b6f377771d18ee4ff8e51d42e52fdf7f
Wesley Hobbie <wslyhbb@hotmail.com>
2023-05-08 15:25:05 -0500

    Adding keyboard navigation to calendar button.

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -1,8 +1,11 @@
 <template>
   <div :class="{'input-group' : bootstrapStyling}">
     <!-- Calendar Button -->
-    <span v-if="calendarButton" class="vdp-datepicker__calendar-button" :class="{'input-group-prepend' : bootstrapStyling}" @click="showCalendar" v-bind:style="{'cursor:not-allowed;' : disabled}">
-      <span :class="{'input-group-text' : bootstrapStyling}">
+    <span v-if="calendarButton" class="vdp-datepicker__calendar-button"
+          :class="{'input-group-prepend' : bootstrapStyling}"
+          :style="{'cursor:not-allowed;' : disabled}" @click="showCalendar"
+          @keyup.enter="showCalendar" @keyup.space="showCalendar">
+      <span :class="{'input-group-text' : bootstrapStyling}" tabindex="0">
         <i :class="calendarButtonIcon">
           {{ calendarButtonIconContent }}
           <span v-if="!calendarButtonIcon">&hellip;</span>
@@ -30,7 +33,8 @@
       @blur="inputBlurred"
       autocomplete="off">
     <!-- Clear Button -->
-    <span v-if="clearButton && selectedDate" class="vdp-datepicker__clear-button" :class="{'input-group-append' : bootstrapStyling}" @click="clearDate()">
+    <span v-if="clearButton && selectedDate" class="vdp-datepicker__clear-button"
+          :class="{'input-group-append' : bootstrapStyling}" @click="clearDate()">
       <span :class="{'input-group-text' : bootstrapStyling}">
         <i :class="clearButtonIcon">
           <span v-if="!clearButtonIcon">&times;</span>
